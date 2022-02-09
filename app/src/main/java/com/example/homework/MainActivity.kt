fc0b860ad8fd3a0a6f0dad21177fc5b63e2f2afa
osipenok3 <96347763+osipenok3@users.noreply.github.com>
2022-02-09 19:43:40 +0300

    Correct 0

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.homework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+import android.net.Uri
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.but1).setOnClickListener {
+            val gmmIntentUri = Uri.parse("google.maps:cbll")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+                startActivity(mapIntent)
+            }
+        findViewById<Button>(R.id.but2).setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:")
+            startActivity(intent)}
     }
 }
