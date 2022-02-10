cb9dd2e798e1f3f511049d347d884a2cca92e561
osipenok3 <96347763+osipenok3@users.noreply.github.com>
2022-02-10 19:32:27 +0300

    additional 0

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import androidx.core.app.ActivityCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +17,22 @@ class MainActivity : AppCompatActivity() {
             val gmmIntentUri = Uri.parse("google.maps:cbll")
             val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
             mapIntent.setPackage("com.google.android.apps.maps")
-                startActivity(mapIntent)
-            }
+            startActivity(mapIntent)
+        }
         findViewById<Button>(R.id.but2).setOnClickListener {
-            val intent = Intent(Intent.ACTION_DIAL)
-            intent.data = Uri.parse("tel:")
-            startActivity(intent)}
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.CALL_PHONE),0)
+            } else {
+                val intent = Intent(Intent.ACTION_CALL)
+                intent.data = Uri.parse("tel:88005553535")
+                startActivity(intent)
+            }
+        }
     }
-}
+    }
